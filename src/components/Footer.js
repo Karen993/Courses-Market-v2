91fabe7b7262f56b88a4c1363d9e82cc81eba177
Elina <kirakosyanelina7@gmail.com>
2021-10-18 21:05:26 +0400

    added searchfield (half done), Store, StoreCmp, CardCmp, GuestHeader, AddToCartButton components (2th variant)

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+export default function Footer() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        height: 50,
+        backgroundColor: "#001428",
+        color: "white",
+        fontSize: 14,
+        textAlign: "center",
+      }}
+    >
+      <p>
+        It's never too late to acquire a new profession. Sign up and find your
+        profession
+      </p>
+    </div>
+  );
+}
